da0ee3693a807e524fff86bb8bb6e7142db4b447
Pasiura Daryna <107425596+Anirad2002@users.noreply.github.com>
2024-03-19 09:52:07 +0100

    Add files via upload

---
+++ Node.JS/zad1/app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const server = http.createServer(requestListener);
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
+  
+const home = require('./views/home');
+const student = require('./views/student');
+   
+app.get('/', (req, res) => {
+    res.send(home.renderPage());
+});
+   
+app.get('/student', (req, res) => {
+    res.send(student.renderPage());
+});
---
+++ Node.JS/zad3/app.js
@@ -0,0 +1,39 @@
+const http = require("http");
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const server = http.createServer(requestListener);
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
+  
+const home = require('./views/home');
+const student = require('./views/student');
+   
+app.get('/', (req, res) => {
+    res.send(home.renderPage());
+});
+   
+app.get('/student', (req, res) => {
+    res.send(student.renderPage());
+});
+
+
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+const { handleHome, handleStudent } = require('./routes');
+
+app.get('/', handleHome);
+app.get('/student', handleStudent);
+
+app.post('/student', (req, res) => {
+    const data = req.body;
+    fs.writeFileSync(`${data.code}.txt`, JSON.stringify(data));
+    res.redirect('/student');
+});
