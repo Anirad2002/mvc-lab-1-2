e468d533925668ed3a593d1289f5816ee3a9202c
Pasiura Daryna <107425596+Anirad2002@users.noreply.github.com>
2024-03-27 08:45:53 +0100

    Update app.js

--- Node.JS/zad3/app.js
+++ Node.JS/zad3/app.js
@@ -4,8 +4,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const server = http.createServer(requestListener);
-
 const server = app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
